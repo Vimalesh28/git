a10a5daf49eb31752c289a3a91ce7eca9a2d62a0
Vimalesh28 <vimaleshkvkl@gmail.com>
2023-01-07 21:31:21 +0530

    added currencyvalue

--- CurrencyFormat.java
+++ CurrencyFormat.java
@@ -1,5 +1,6 @@
 //https://www.hackerrank.com/challenges/java-currency-formatter/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 package samm;
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -7,30 +8,22 @@ import java.math.*;
 import java.util.regex.*;
 
 public class CurrencyFormat {
-    
-    public static void main(String[] args) {
-    	Scanner scanner =new Scanner(System.in);
-    	
-    	HashMap hashmap=new HashMap<>();
-    	double payment = scanner.nextDouble();
-        
-      NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
-        
-      hashmap.put(1,"us "+n.format(payment));
-        
-       NumberFormat n1 = NumberFormat.getCurrencyInstance(new Locale("CH", "IN"));
-       hashmap.put(2,"INDIA"+n1.format(payment));
-       
-        NumberFormat n2 = NumberFormat.getCurrencyInstance(Locale.CHINA);
-        hashmap.put(3,"CHINA"+n2.format(payment));
-        
-        NumberFormat n3 = NumberFormat.getCurrencyInstance(Locale.FRANCE);
-        hashmap.put(4,"FRANCE"+n3.format(payment));
-        
-        
-         System.out.println(hashmap);
-    	
-    	
-    
-    }
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		HashMap hashmap = new HashMap<>();
+		double payment = scanner.nextDouble();
+
+		hashmap.put("us", NumberFormat.getCurrencyInstance(Locale.US).format(payment));
+
+		hashmap.put("INDIA", NumberFormat.getCurrencyInstance(new Locale("CH", "IN")).format(payment));
+
+		hashmap.put("CHINA", NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment));
+
+		hashmap.put("FRANCE", NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment));
+
+		System.out.println(hashmap);
+
+	}
 }
